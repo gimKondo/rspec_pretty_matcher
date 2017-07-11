d5112c4f35088ce8d35ecbf9b7e1aa1d4e8deba6
gim_kondo <gim0kondo@gmail.com>
2017-07-11 06:59:16 +0000

    implement const_eq

--- lib/rspec_pretty_matcher.rb
+++ lib/rspec_pretty_matcher.rb
@@ -1,5 +1,20 @@
+require "bundler"
+Bundler.require
 require "rspec_pretty_matcher/version"
 
 module RspecPrettyMatcher
-  # Your code goes here...
+  # matcher for constants
+  RSpec::Matchers.define :const_eq do |mozule, exp_symbol|
+    match do |act_val|
+      mozule.const_get(exp_symbol) == act_val
+    end
+    failure_message do |act_val|
+      act_symbol = mozule.constants(true).find { |c| mozule.const_get(c) == act_val }
+      act_msg = if act_symbol.nil? then "     got: unknown value (#{act_val})"
+                else "     got: #{mozule.name}::#{act_symbol} (#{act_val})"
+                end
+      exp_val = mozule.const_get(exp_symbol)
+      "expected: #{mozule.name}::#{exp_symbol} (#{exp_val})\n" << act_msg
+    end
+  end
 end
--- spec/rspec_pretty_matcher_spec.rb
+++ spec/rspec_pretty_matcher_spec.rb
@@ -1,11 +1,28 @@
 require "spec_helper"
 
+# Sample module
+module ErrorCode
+  SUCCESS = 0
+  ERROR   = -1
+end
+
 RSpec.describe RspecPrettyMatcher do
   it "has a version number" do
     expect(RspecPrettyMatcher::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "Success to match" do
+    code = ErrorCode::SUCCESS
+    expect(code).to const_eq ErrorCode, :SUCCESS
+  end
+
+  it "Fail to match" do
+    code = ErrorCode::ERROR
+    expect(code).to const_eq ErrorCode, :SUCCESS
+  end
+
+  it "Fail to match(unknown value)" do
+    code = -3
+    expect(code).to const_eq ErrorCode, :SUCCESS
   end
 end
